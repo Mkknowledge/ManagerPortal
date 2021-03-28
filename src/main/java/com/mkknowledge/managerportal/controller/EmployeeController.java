084c64c3c67c8d7c4ff26f3beba2604db77c12e7
Mkknowledge <kmayur2124@gmail.com>
2021-03-28 16:44:13 +0530

    Added EmployeeModel and EmployeeController

---
+++ src/main/java/com/mkknowledge/managerportal/controller/EmployeeController.java
@@ -0,0 +1,59 @@
+package com.mkknowledge.managerportal.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import com.mkknowledge.managerportal.model.Employee;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class EmployeeController {
+	
+	private List<Employee> employees = createList();
+	
+	@RequestMapping(value = "/employees", method = RequestMethod.GET, produces = "application/json")
+	public List<Employee> listEmployee() {
+		return employees;
+	}
+	
+	private static List<Employee> createList() {
+		
+		String pattern = "MM/dd/yyyy";
+	    SimpleDateFormat format = new SimpleDateFormat(pattern);
+		List<Employee> tempEmployees = new ArrayList<>();
+		Employee emp1 = new Employee();
+		emp1.setEmpId("1");
+		emp1.setFirstname("Mayur");
+		emp1.setLastname("Kandalkar");
+		emp1.setAddress("Nanded City");
+		try {
+			emp1.setDob(format.parse("21/06/1992"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		Employee emp2 = new Employee();
+		emp2.setEmpId("2");
+		emp2.setFirstname("Shital");
+		emp2.setLastname("Mohite");
+		emp2.setAddress("Shivne");
+		try {
+			emp2.setDob(format.parse("02/08/1994"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		tempEmployees.add(emp1);
+		tempEmployees.add(emp2);
+		return tempEmployees;
+	}
+
+}
---
+++ src/main/java/com/mkknowledge/managerportal/model/Employee.java
@@ -0,0 +1,137 @@
+package com.mkknowledge.managerportal.model;
+
+import java.util.Date;
+
+public class Employee {
+	
+	private String empId;
+	private String firstname;
+	private String lastname;
+	private String address;
+	private Date dob;
+	private String mobile;
+	private String city;
+	
+	public Employee() {
+		
+	}
+
+	public String getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((dob == null) ? 0 : dob.hashCode());
+		result = prime * result + ((empId == null) ? 0 : empId.hashCode());
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+		result = prime * result + ((mobile == null) ? 0 : mobile.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (dob == null) {
+			if (other.dob != null)
+				return false;
+		} else if (!dob.equals(other.dob))
+			return false;
+		if (empId == null) {
+			if (other.empId != null)
+				return false;
+		} else if (!empId.equals(other.empId))
+			return false;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (lastname == null) {
+			if (other.lastname != null)
+				return false;
+		} else if (!lastname.equals(other.lastname))
+			return false;
+		if (mobile == null) {
+			if (other.mobile != null)
+				return false;
+		} else if (!mobile.equals(other.mobile))
+			return false;
+		return true;
+	}
+	
+	
+}
