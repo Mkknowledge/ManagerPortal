4f7d340c989a1be9995da255295199ab90808441
Mkknowledge <kmayur2124@gmail.com>
2021-03-28 22:28:26 +0530

    bug fixes

--- src/main/java/com/mkknowledge/managerportal/controller/EmployeeController.java
+++ src/main/java/com/mkknowledge/managerportal/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,11 +20,12 @@ import com.mkknowledge.managerportal.model.Employee;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping({ "/employees" })
 public class EmployeeController {
 	
 	private List<Employee> employees = createList();
 	
-	@RequestMapping(value = "/employees", method = RequestMethod.GET, produces = "application/json")
+	@GetMapping(produces = "application/json")
 	public List<Employee> listEmployee() {
 		return employees;
 	}
