b5ee3c92484e825413c0bba620a83285842ff1a4
Mkknowledge <kmayur2124@gmail.com>
2021-03-28 15:57:10 +0530

    first Commit

---
+++ src/main/java/com/mkknowledge/managerportal/ManagerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.mkknowledge.managerportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkknowledge/managerportal/ManagerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkknowledge.managerportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
