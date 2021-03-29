272830113ffdc9bc801b66ac2962f4f8e432ba1e
Mkknowledge <kmayur2124@gmail.com>
2021-03-29 16:25:25 +0530

    JWT classes added

---
+++ src/main/java/com/mkknowledge/managerportal/service/ManagerDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.mkknowledge.managerportal.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.mkknowledge.managerportal.model.Manager;
+import com.mkknowledge.managerportal.repository.ManagerRepository;
+
+public class ManagerDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	ManagerRepository managerRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Manager manager = managerRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+		return ManagerDetailsImpl.build(manager);
+	}
+}
